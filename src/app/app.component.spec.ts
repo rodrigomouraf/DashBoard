029c2e23de82c6c645551ae327341c6323e67f74
Rodrigo Moura <rodrigomoura_rm@hotmail.com>
2019-02-21 16:34:23 -0300

    Versão1.0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        DashboardModule
       ],
     }).compileComponents();
   }));
